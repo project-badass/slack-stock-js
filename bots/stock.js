12567f7375ca2ceaefaa04ea8392c1443ccb7213
Ryan Hartzfeld <ryan.hartzfeld@coxautoinc.com>
2018-11-30 11:02:22 -0500

    Sort them stahcks (and use ES6 to do it)

--- bots/stock.js
+++ bots/stock.js
@@ -1,60 +1,48 @@
+const Wreck = require('wreck');
+
+const BASE_URL = 'https://api.iextrading.com/1.0/stock/market/batch?types=quote&symbols=';
+
+// Slack Payload Cheatsheet
+// {
+//    token=gT0R4UDPjbBzvYSll5aPtS6Z
+//    team_id=T0001
+//    team_domain=example
+//    channel_id=C2147483705
+//    channel_name=test
+//    timestamp=1355517523.000005
+//    user_id=U2147483697
+//    user_name=Steve
+//    text=googlebot: What is the air-speed velocity of an unladen swallow?
+//    trigger_word=googlebot:
+// }
+
 module.exports = {
   path:    '/stock',
-  handler: function(request, reply) {
+  handler: (request, reply) => {
+    const msg = request.payload;
 
-    // Slack Payload Cheatsheet
-    // {
-    //    token=gT0R4UDPjbBzvYSll5aPtS6Z
-    //    team_id=T0001
-    //    team_domain=example
-    //    channel_id=C2147483705
-    //    channel_name=test
-    //    timestamp=1355517523.000005
-    //    user_id=U2147483697
-    //    user_name=Steve
-    //    text=googlebot: What is the air-speed velocity of an unladen swallow?
-    //    trigger_word=googlebot:
-    // }
-    var Wreck = require('wreck');
-    var msg = request.payload;
+    // find words prefixed with `$`
+    const matches = msg.text.match(/\$[A-Za-z.]+/g);
 
-    // Suppose you only want to respond to messages that match a certain criteria
-    var matches = msg.text.match(/\$[A-Za-z.]+/g);
-    if (matches) {
-      var symbols = [];
-      url = 'https://api.iextrading.com/1.0/stock/market/batch?types=quote&symbols=';
-      for (var i = 0; i < matches.length; i++) {
-        if (i > 0) {
-          url += ',';
-        }
-        
-        var symbol = matches[i].replace('$', '').toUpperCase();
-        url += symbol;
-        symbols.push(symbol);
-      }
+    if (matches && matches.length) {
+      const symbols = matches.map(mapSymbol);
+      const url = BASE_URL + symbols.join(',');
       
-      Wreck.get(url, function (err, res, payload) {
+      Wreck.get(url, (err, res, payload) => {
         if (!err) {
-          var json = JSON.parse(payload);
-          var text = '';
-          
-          for (var i = 0; i < symbols.length; i++) {
-            if (i > 0) {
-              text += '\n';
-            }
-            
-            var symbol = symbols[i];
-            var stockResponse = json[symbol];
-            if (stockResponse.quote) {
-              text += formatQuote(stockResponse.quote);
-            } else {
-              text += formatQuote(null);
-            }
-          }
+          const json = JSON.parse(payload);
 
-          return reply({
-            'text': text
-          }).code(200);
+          const text = symbols
+            .map((symbol) =>
+              (json[symbol] && json[symbol].quote)
+                ? json[symbol].quote
+                : { symbol }
+            )
+            .sort(sortQuote)
+            .map(formatQuote)
+            .join('\n');
+
+          return reply({ text }).code(200);
         }
       });
     } else {
@@ -64,21 +52,38 @@ module.exports = {
   }
 };
 
-var formatQuote = function(quote) {
-  if (quote) {
-    var change = quote.change
+const mapSymbol = (raw) => {
+  return raw.replace('$', '').toUpperCase();
+};
+
+const sortQuote = (quoteA, quoteB) => {
+  if (!quoteA.changePercent && !quoteB.changePercent) { return 0; }
+  if (!quoteA.changePercent) { return 1; }
+  if (!quoteB.changePercent) { return -1; }
+  if (quoteA.changePercent < quoteB.changePercent) {
+    return 1;
+  }
+  if (quoteA.changePercent > quoteB.changePercent) {
+    return -1;
+  }
+  return 0;
+};
+
+const formatQuote = (quote) => {
+  if (quote.change) {
+    const change = quote.change
+    let emoji;
     if (change == 0) {
       // equal
       emoji = ':point_right:';
     } else if (change > 0) {
-      // gains
+      // gainz
       emoji = ':point_up_2:'
     } else {
       // losses
       emoji = ':point_down:';
     }
-    return emoji + ' *' + quote.companyName + '* (' + quote.symbol + '): ' + (Math.round(quote.latestPrice * 100) / 100) + ' (_' + change + ' ' + (Math.round(quote.changePercent * 10000) / 100) + '%_)';
+    return `${emoji} *${quote.companyName}* (${quote.symbol}): ${(Math.round(quote.latestPrice * 100) / 100)} (_${change} ${Math.round(quote.changePercent * 10000) / 100}%_)`
   }
-
-  return ':question: *' + quote.symbol + '*: Symbol not found or quote unavailable';
-}
+  return `:question: *${quote.symbol}*: Symbol not found or quote unavailable`;
+};
